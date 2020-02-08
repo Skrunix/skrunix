1d1b9a9c18f3fd1c85e094a3e58252cd4a71099e
David Skrundz <david@skrundz.ca>
2020-02-07 22:58:22 -0700

    Kernel: Store the Screen globally for later

--- Kernel/src/kmain.cpp
+++ Kernel/src/kmain.cpp
@@ -1,41 +1,45 @@
 #include "Screen.hpp"
 
+uint8_t screenData[sizeof(Screen)];
+Screen* screen;
+
 void main() {
-	Screen screen;
-	screen.Clear();
+	screen  = (Screen*)&screenData;
+	*screen = Screen();
+	screen->Clear();
 
-	screen.SetForeground(Screen::Color::Red);
-	screen.setBackground(Screen::Color::Black);
-	screen.Write("\nSkrunix\r\n\n");
-	screen.ScrollUp();
+	screen->SetForeground(Screen::Color::Red);
+	screen->setBackground(Screen::Color::Black);
+	screen->Write("\nSkrunix\r\n\n");
+	screen->ScrollUp();
 
-	screen.SetForeground(Screen::Color::Blue);
-	screen.setBackground(Screen::Color::Red);
-	screen.WriteRaw("Character test:");
+	screen->SetForeground(Screen::Color::Blue);
+	screen->setBackground(Screen::Color::Red);
+	screen->WriteRaw("Character test:");
 
-	screen.SetForeground(Screen::Color::Magenta);
-	screen.setBackground(Screen::Color::Cyan);
-	screen.WriteRaw('\0');
+	screen->SetForeground(Screen::Color::Magenta);
+	screen->setBackground(Screen::Color::Cyan);
+	screen->WriteRaw('\0');
 	for (char i = 1; i != 0; ++i) {
-		screen.WriteRaw(i);
+		screen->WriteRaw(i);
 	}
 
-	screen.Write("\r\n\n");
-	screen.SetForeground(Screen::Color::Blue);
-	screen.setBackground(Screen::Color::LightRed);
-	screen.WriteRaw("Color test:");
+	screen->Write("\r\n\n");
+	screen->SetForeground(Screen::Color::Blue);
+	screen->setBackground(Screen::Color::LightRed);
+	screen->WriteRaw("Color test:");
 	for (char fg = 0; fg < 0x10; ++fg) {
 		for (char bg = 0; bg < 0x10; ++bg) {
-			screen.SetForeground((Screen::Color)fg);
-			screen.setBackground((Screen::Color)bg);
-			screen.WriteRaw('E');
+			screen->SetForeground((Screen::Color)fg);
+			screen->setBackground((Screen::Color)bg);
+			screen->WriteRaw('E');
 		}
 	}
 
-	screen.SetForeground(Screen::Color::LightGray);
-	screen.setBackground(Screen::Color::Black);
-	screen.Write("\r\n\n");
-	screen.WriteHex((uintptr_t)&screen);
+	screen->SetForeground(Screen::Color::LightGray);
+	screen->setBackground(Screen::Color::Black);
+	screen->Write("\r\n\n");
+	screen->WriteHex((uintptr_t)screen);
 }
 
 extern "C" {
