2f3e1791a1dd2dcd077060523e04227b420991c2
David Skrundz <david@skrundz.ca>
2020-02-16 18:32:53 -0700

    GDT code cleanup

--- Kernel/src/GDT.cpp
+++ Kernel/src/GDT.cpp
@@ -1,13 +1,17 @@
 #include "GDT.hpp"
 
-extern "C" {
-typedef struct {
-	uint16_t  limit;
-	uintptr_t base;
-} __attribute__((packed)) GDTPointer;
-static_assert(sizeof(GDTPointer) == 10);
+#define ACCESS_PRESENT (0b1 << 7)
+#define ACCESS_PRIV_KERNEL (0b00 << 5)
+#define ACCESS_TYPE_CODE (0b11 << 3)
+#define ACCESS_TYPE_DATA (0b10 << 3)
+#define ACCESS_GROW_UP (0b0 << 2)
+#define ACCESS_RW (0b1 << 1)
 
-typedef struct {
+#define FLAGS_GRANULARITY_4K (0b1 << 3)
+#define FLAGS_SIZE_64_CODE (0b01 << 1)
+#define FLAGS_SIZE_64_DATA (0b00 << 1)
+
+struct __attribute__((packed)) GDTEntry {
 	uint16_t limitLow;      // Limit 0:15
 	uint16_t baseLow;       // Base 0:15
 	uint8_t  baseMid;       // Base 16:23
@@ -15,9 +19,16 @@ typedef struct {
 	uint8_t  limitHigh : 4; // Limit 16:19
 	uint8_t  flags : 4;     // Flags
 	uint8_t  baseHigh;      // Base 24:31
-} __attribute__((packed)) GDTEntry;
+};
 static_assert(sizeof(GDTEntry) == 8);
 
+struct __attribute__((packed)) GDTPointer {
+	uint16_t  limit;
+	GDTEntry* base;
+};
+static_assert(sizeof(GDTPointer) == 10);
+
+extern "C" {
 extern void loadGDT(GDTPointer*);
 }
 
@@ -26,11 +37,20 @@ GDTEntry   gdtEntries[3];
 
 GDT::GDT() {
 	gdt.limit = sizeof(gdtEntries) - 1;
-	gdt.base  = reinterpret_cast<uintptr_t>(&gdtEntries[0]);
+	gdt.base  = &gdtEntries[0];
 
-	this->SetEntry(0, 0x1FFFF, 0, 0, 0);               // Null
-	this->SetEntry(1, 0xFFFFF, 0, 0b10011010, 0b1010); // Code
-	this->SetEntry(2, 0xFFFF, 0, 0b10010010, 0b0000);  // Data
+	uint8_t access =
+	    ACCESS_PRESENT | ACCESS_PRIV_KERNEL | ACCESS_GROW_UP | ACCESS_RW;
+	uint8_t accessCode = access | ACCESS_TYPE_CODE;
+	uint8_t accessData = access | ACCESS_TYPE_DATA;
+
+	uint8_t flags     = FLAGS_GRANULARITY_4K;
+	uint8_t flagsCode = flags | FLAGS_SIZE_64_CODE;
+	uint8_t flagsData = flags | FLAGS_SIZE_64_DATA;
+
+	this->SetEntry(0, UINT32_MAX, 0, 0, 0);                  // Null
+	this->SetEntry(1, UINT32_MAX, 0, accessCode, flagsCode); // Code
+	this->SetEntry(2, UINT32_MAX, 0, accessData, flagsData); // Data
 
 	loadGDT(&gdt);
 }
