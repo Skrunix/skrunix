e4bca9be8b503276100bd5d909b65f5248422d45
David Skrundz <david@skrundz.ca>
2020-02-19 20:49:59 -0700

    Bootloader: Correctly detect memory using the BIOS

--- Kernel/src/main.cpp
+++ Kernel/src/main.cpp
@@ -86,6 +86,12 @@ void main() {
 			screen->Write("Usable");
 		} else if (range.type == 2) {
 			screen->Write("Reserved");
+		} else if (range.type == 3) {
+			screen->Write("ACPI reclaimable");
+		} else if (range.type == 4) {
+			screen->Write("ACPI NVS");
+		} else if (range.type == 4) {
+			screen->Write("Bad");
 		} else {
 			screen->Write("? ");
 			screen->WriteHex(static_cast<uint8_t>(range.type));
