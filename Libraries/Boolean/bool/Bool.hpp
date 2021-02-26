6b078cdf74c4280115b21a611757794ec758ce45
David Skrundz <david@skrundz.ca>
2021-02-25 21:22:15 -0700

    Boolean: Redo it all for better safety

--- Libraries/Boolean/bool/Bool.hpp
+++ Libraries/Boolean/bool/Bool.hpp
@@ -1,75 +1,76 @@
 #pragma once
 
+#include <concept/same_as.hpp>
+#include <type_trait/is_nothrow_convertible.hpp>
+
 struct [[gnu::packed]] alignas(alignof(bool)) Bool {
-	Bool()    = delete;
-	Bool(int) = delete;
-
-	[[gnu::always_inline]] inline constexpr Bool(bool value) noexcept
-	    : value(value){};
-
-	[[gnu::always_inline]] inline constexpr explicit operator bool()
-	    const noexcept {
-		return this->value;
-	}
-
   private:
 	bool value;
+
+	struct P_ {
+		bool V_;
+	};
+	typedef const bool P_::*bool_;
+
+  public:
+	[[gnu::always_inline]] inline constexpr Bool(const Bool& b) noexcept
+	    : value(b.value) {}
+
+	template <std::same_as<bool> B>
+	[[gnu::always_inline]] inline constexpr Bool(const B b) noexcept
+	    : value(static_cast<bool>(b)) {}
+
+	[[gnu::always_inline]] inline constexpr operator bool_() const noexcept {
+		return this->value ? &P_::V_ : 0;
+	}
 };
-static_assert(sizeof(Bool) == sizeof(bool));
-static_assert(alignof(Bool) == alignof(bool));
 
 [[gnu::always_inline]] inline constexpr Bool
 operator!(const Bool& value) noexcept {
-	return !static_cast<bool>(value);
+	return Bool(!static_cast<bool>(value));
 }
 
 [[gnu::always_inline]] inline constexpr Bool
 operator==(const Bool& lhs, const Bool& rhs) noexcept {
-	return static_cast<bool>(lhs) == static_cast<bool>(rhs);
+	return Bool(static_cast<bool>(lhs) == static_cast<bool>(rhs));
 }
+
 [[gnu::always_inline]] inline constexpr Bool
 operator!=(const Bool& lhs, const Bool& rhs) noexcept {
-	return static_cast<bool>(lhs) != static_cast<bool>(rhs);
-}
-[[gnu::always_inline]] inline constexpr Bool
-operator&&(const Bool& lhs, const Bool& rhs) noexcept {
-	return static_cast<bool>(lhs) && static_cast<bool>(rhs);
-}
-[[gnu::always_inline]] inline constexpr Bool
-operator||(const Bool& lhs, const Bool& rhs) noexcept {
-	return static_cast<bool>(lhs) || static_cast<bool>(rhs);
+	return Bool(static_cast<bool>(lhs) != static_cast<bool>(rhs));
 }
 
-[[gnu::always_inline]] inline constexpr Bool
-operator==(const Bool& lhs, const bool& rhs) noexcept {
-	return static_cast<bool>(lhs) == static_cast<bool>(rhs);
-}
-[[gnu::always_inline]] inline constexpr Bool
-operator!=(const Bool& lhs, const bool& rhs) noexcept {
-	return static_cast<bool>(lhs) != static_cast<bool>(rhs);
-}
-[[gnu::always_inline]] inline constexpr Bool
-operator&&(const Bool& lhs, const bool& rhs) noexcept {
-	return static_cast<bool>(lhs) && static_cast<bool>(rhs);
-}
-[[gnu::always_inline]] inline constexpr Bool
-operator||(const Bool& lhs, const bool& rhs) noexcept {
-	return static_cast<bool>(lhs) || static_cast<bool>(rhs);
-}
+// Size and Alignment
+static_assert(sizeof(Bool) == sizeof(bool));
+static_assert(alignof(Bool) == alignof(bool));
 
-[[gnu::always_inline]] inline constexpr Bool
-operator==(const bool& lhs, const Bool& rhs) noexcept {
-	return static_cast<bool>(lhs) == static_cast<bool>(rhs);
-}
-[[gnu::always_inline]] inline constexpr Bool
-operator!=(const bool& lhs, const Bool& rhs) noexcept {
-	return static_cast<bool>(lhs) != static_cast<bool>(rhs);
-}
-[[gnu::always_inline]] inline constexpr Bool
-operator&&(const bool& lhs, const Bool& rhs) noexcept {
-	return static_cast<bool>(lhs) && static_cast<bool>(rhs);
-}
-[[gnu::always_inline]] inline constexpr Bool
-operator||(const bool& lhs, const Bool& rhs) noexcept {
-	return static_cast<bool>(lhs) || static_cast<bool>(rhs);
-}
+// Constructor
+static_assert(Bool(true));
+
+// Negation
+static_assert(!Bool(true) == false);
+static_assert(!Bool(false) == true);
+
+// Equality
+static_assert(Bool(true) == true);
+static_assert(Bool(false) == false);
+static_assert(Bool(true) == Bool(true));
+static_assert(Bool(false) == Bool(false));
+
+// Inequality
+static_assert(Bool(true) != false);
+static_assert(Bool(false) != true);
+static_assert(Bool(true) != Bool(false));
+static_assert(Bool(false) != Bool(true));
+
+// Conversion to and from bool
+static_assert(std::is_nothrow_convertible_v<bool, Bool>);
+static_assert(std::is_nothrow_convertible_v<Bool, bool>);
+
+// Conversion to and from int, float, void*
+static_assert(!std::is_nothrow_convertible_v<int, Bool>);
+static_assert(!std::is_nothrow_convertible_v<Bool, int>);
+static_assert(!std::is_nothrow_convertible_v<float, Bool>);
+static_assert(!std::is_nothrow_convertible_v<Bool, float>);
+static_assert(!std::is_nothrow_convertible_v<void*, Bool>);
+static_assert(!std::is_nothrow_convertible_v<Bool, void*>);
